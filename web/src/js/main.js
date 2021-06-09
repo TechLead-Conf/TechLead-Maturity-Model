60e9742dc066eac2990d84cc7d7a752d97cdf92d
Viet Nguyen <me@tuanviet.ru>
2021-06-10 00:02:15 +0300

    feat: show description tooltips

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,2 +1,6 @@
-require("normalize.css");
-// require("bulma/css/bulma");
+import "normalize.css";
+
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+
+tippy("[data-tippy-content]", { delay: 50, inertia: true, interactive: true });
